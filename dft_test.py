29c281956625959e0cf48ba156031cd9651bfb7c
Hanson-Sun <65196939+Hanson-Sun@users.noreply.github.com>
2022-03-05 00:09:13 -0800

    dual channel support
    ok so it still doesnt work, but at least theres dual channel now

--- dft_test.py
+++ dft_test.py
@@ -6,6 +6,7 @@ separation of real component and imaginary component for x and y values
 import math
 import numpy as np
 from scipy.io import wavfile
+from pydub import AudioSegment
 
 #implementation of the DFT algorithm from wikipedia
 def DFT(x):
@@ -45,41 +46,41 @@ test_points_x = [x[0] for x in test_points]
 test_points_y = [x[1] for x in test_points]
 
 wave_info_x = DFT(test_points_x)
-
 wave_info_x = map_amplitude(wave_info_x)
 
-print(wave_info_x)
+wave_info_y = DFT(test_points_y)
+wave_info_y = map_amplitude(wave_info_y)
 
-#samples per second
 sps = 44100
 
-T = 1/ sps
+def generate_mono_sound(x):
+    #samples per second
+    
+    T = 1/ sps
+    #frequency scaling factor
+    freq_base = 440.0 # i think this is A
+    duration = 5.0 # this is in seconds
+    N = sps * duration #sample size
+    waveform_quiet = np.arange(N)*T
+    for wave in x:
+        sample_number = np.arange(N)*T
+        omega = 2 * np.pi * freq_base * wave["freq"]
+        phase = wave["phase"]
+        waveform = np.sin(omega * sample_number + phase)
+        waveform_quiet += waveform * wave["amplitude"]
 
-#frequency scaling factor
-freq_base = 440.0 # i think this is A
+    waveform_integers = np.int16(waveform_quiet * 32767)
 
-duration = 5.0 # this is in seconds
-
-N = sps * duration #sample size
-
-
-waveform_quiet = np.arange(N)*T
-
-for wave in wave_info_x:
-    sample_number = np.arange(N)*T
-
-    omega = 2 * np.pi * freq_base * wave["freq"]
-
-    phase = wave["phase"]
-
-    waveform = np.sin(omega * sample_number + phase)
-
-    waveform_quiet += waveform * wave["amplitude"]
+    return waveform_integers
 
 
 
+wavfile.write("sound_x.wav", sps, generate_mono_sound(wave_info_x))
+wavfile.write("sound_y.wav", sps, generate_mono_sound(wave_info_y))
 
-waveform_integers = np.int16(waveform_quiet * 32767)
+left_channel = AudioSegment.from_wav("sound_x.wav")
+right_channel = AudioSegment.from_wav("sound_y.wav")
 
-wavfile.write("lol.wav", sps, waveform_integers)
+stereo_sound = AudioSegment.from_mono_audiosegments(left_channel, right_channel)
 
+stereo_sound.export("final.wav", format="wav")
