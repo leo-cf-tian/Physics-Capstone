881c0a8cfc6b4942993928f71d40fe2951105047
Hanson-Sun <65196939+Hanson-Sun@users.noreply.github.com>
2022-03-04 23:50:43 -0800

    1 channel wave generation
    no way it works for the x values, just need to figure out how to make it dual channel now

--- dft_test.py
+++ dft_test.py
@@ -30,27 +30,56 @@ def DFT(x):
 
     return X_final
 
+def map_amplitude(x):
+    previ = -1
+    for i in x:
+        if i["amplitude"] > previ:
+            previ = i["amplitude"]
+    for i in x:
+        i["amplitude"] = i["amplitude"]/previ
+    return x
+
+
 test_points = [[0, 100], [0, 50], [0, 0], [100, 100], [100, 50], [100, 0], [50, 100], [50, 0]] #should be a square idk
 test_points_x = [x[0] for x in test_points]
 test_points_y = [x[1] for x in test_points]
 
-print(DFT(test_points_x))
+wave_info_x = DFT(test_points_x)
+
+wave_info_x = map_amplitude(wave_info_x)
+
+print(wave_info_x)
 
 #samples per second
 sps = 44100
+
+T = 1/ sps
 
 #frequency scaling factor
 freq_base = 440.0 # i think this is A
 
 duration = 5.0 # this is in seconds
 
-sample_number = np.arrange(duration * sps)
+N = sps * duration #sample size
 
-waveform = np.sin(2 * np.pi * sample_number * freq_base / sps)
 
-waveform_quiet = waveform * 0.3
+waveform_quiet = np.arange(N)*T
+
+for wave in wave_info_x:
+    sample_number = np.arange(N)*T
+
+    omega = 2 * np.pi * freq_base * wave["freq"]
+
+    phase = wave["phase"]
+
+    waveform = np.sin(omega * sample_number + phase)
+
+    waveform_quiet += waveform * wave["amplitude"]
+
+
+
 
 waveform_integers = np.int16(waveform_quiet * 32767)
 
-wavfile.write("soundlmao.wav", sps, waveform_integers)
+wavfile.write("lol.wav", sps, waveform_integers)
 
