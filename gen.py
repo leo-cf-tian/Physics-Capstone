db9da2281f6207f5effcb4757284c2606ead33d7
Midnight417 <69664426+Midnight417@users.noreply.github.com>
2022-03-09 19:37:18 -0800

    add progress bar

--- gen.py
+++ gen.py
@@ -3,6 +3,7 @@ import numpy as np
 import mediapipe as mp
 from sklearn.neighbors import NearestNeighbors
 import networkx as nx
+import imutils
 
 
 """
@@ -11,7 +12,7 @@ This list is ordered to form a path
 """
 
 
-def get_edges(frame):
+def get_edges(frame, blur, detect):
     # initialize mediapipe
     mp_selfie_segmentation = mp.solutions.selfie_segmentation
     selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation(
@@ -21,47 +22,50 @@ def get_edges(frame):
     frame = cv2.resize(frame, [200, int(200/width*height)])
     height, width, channel = frame.shape
 
-    # bg_image = cv2.imread('input/white.jpg')
+    if detect:
+        bg_image = cv2.imread('input/white.jpg')
 
-    # RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    # # get the result
-    # results = selfie_segmentation.process(RGB)
-    # # extract segmented mask
-    # mask = results.segmentation_mask
+        RGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # get the result
+        results = selfie_segmentation.process(RGB)
+        # extract segmented mask
+        mask = results.segmentation_mask
 
-    # # it returns true or false where the condition applies in the mask
-    # # it returns true or false where the condition applies in the mask
-    # condition = np.stack((mask,) * 3, axis=-1) > 0.5
-    # # resize the background image to the same size of the original frame
-    # bg_image = cv2.resize(bg_image, (width, height))
+        # it returns true or false where the condition applies in the mask
+        # it returns true or false where the condition applies in the mask
+        condition = np.stack((mask,) * 3, axis=-1) > 0.5
+        # resize the background image to the same size of the original frame
+        bg_image = cv2.resize(bg_image, (width, height))
 
-    # # combine frame and background image using the condition
-    # output_image = np.where(condition, frame, bg_image)
+        # combine frame and background image using the condition
+        frame = np.where(condition, frame, bg_image)
 
-    # edges = cv2.Canny(cv2.GaussianBlur(frame, [0, 0], 1, 1), 100, 150)
-    edges = cv2.Canny(frame, 100, 150)
+    if blur:
+        frame = cv2.GaussianBlur(frame, [0, 0], 1, 1)
+    
+    edges = imutils.auto_canny(frame)
 
     # Prints coordinates of white pixels
     y, x = np.where(edges == [255])
 
     points = np.c_[x, y]
 
-    if len(points) > 16:
-        clf = NearestNeighbors(n_neighbors=15).fit(points)
-        G = clf.kneighbors_graph()
+    if len(points) <= 16:
+        return []
+    
+    clf = NearestNeighbors(n_neighbors=15).fit(points)
+    G = clf.kneighbors_graph()
 
-        T = nx.from_scipy_sparse_array(G)
+    T = nx.from_scipy_sparse_array(G)
 
-        order = list(nx.dfs_preorder_nodes(T, 0))
+    order = list(nx.dfs_preorder_nodes(T, 0))
 
-        xx = x[order] - width / 2
-        yy = height / 2 - y[order]
+    xx = x[order] - width / 2
+    yy = height / 2 - y[order]
 
-        coord = np.column_stack([xx, yy])
+    coord = np.column_stack([xx, yy])
 
-        return coord
-
-    return []
+    return coord
 
 """
 Inverse Fourier Transform
@@ -117,5 +121,27 @@ def map_amplitude(x):
     x = x/np.abs(previ)
     return x
 
-def encode(frame):
-    return ifft(get_edges(frame))
+def encode(frame, blur, detect):
+    return ifft(get_edges(frame, blur, detect))
+
+
+def count_frames_manual(video):
+    total = 0
+    while True:
+        (grabbed, frame) = video.read()
+        if not grabbed:
+            break
+        total += 1
+    return total
+
+def count_frames(cap, override=False):
+    total = 0
+    if override:
+        total = count_frames_manual(cap)
+    else:
+        try:
+            total = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+        except:
+            total = count_frames_manual(cap)
+    cap.release()
+    return total
--- main.py
+++ main.py
@@ -3,12 +3,23 @@ import numpy as np
 from scipy.io import wavfile
 from pydub import AudioSegment
 import cv2
+import imutils
+import math
 
 file = input("File to encode: ")
+blur = input("Blur? (Y/N): ").capitalize()
+while not (blur == "Y" or blur == "N"):
+    blur = input("Blur? (Y/N): ").capitalize()
+blur = blur == "Y"
+
+detect = input("Crop Background? (Y/N): ").capitalize()
+while not (detect == "Y" or detect == "N"):
+    detect = input("Crop Background? (Y/N): ").capitalize()
+detect = detect == "Y"
 
 if file.endswith((".jpg", ".png")):
     frame = cv2.imread(file)
-    x, y = gen.encode(frame)
+    x, y = gen.encode(frame, blur, detect)
 
     repeat = 1000
 
@@ -20,15 +31,20 @@ if file.endswith((".jpg", ".png")):
     stereo_sound = AudioSegment.from_mono_audiosegments(left_channel, right_channel)
     stereo_sound.export("output/final_sound.wav", format="wav")
 
-elif file.endswith((".mp4")):
+elif file.endswith((".mp4", ".avi")):
     
     cap = cv2.VideoCapture(file)
+    
+    num = gen.count_frames(cap)
+    
+    cap = cv2.VideoCapture(file)
+    
     success,frame = cap.read()
     count = 0
     while success:
         if (count % 1 == 0):
     
-            x, y = gen.encode(frame)
+            x, y = gen.encode(frame, blur, detect)
             
             wavfile.write("output/sound_wave_x.wav", 44100, np.int16(x * 32767))
             wavfile.write("output/sound_wave_y.wav", 44100, np.int16(y * 32767))
@@ -38,6 +54,13 @@ elif file.endswith((".mp4")):
                 stereo_sound = AudioSegment.from_mono_audiosegments(left_channel, right_channel)
             else:
                 stereo_sound += AudioSegment.from_mono_audiosegments(left_channel, right_channel)
+            
         success,frame = cap.read()
+        
         count += 1
+        
+        progress = math.ceil(count / num * 20)
+        print("Progress: |" + "█" * progress + " " * (20 - progress) + "| " + str(round(count / num * 100)) + "% (" + str(count) + "/" + str(num) + ")", end='\r')
+        
+    print("\nFinished Rendering!")
     stereo_sound.export("output/final_sound.wav", format="wav")
